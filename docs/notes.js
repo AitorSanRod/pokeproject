2070b5bc2165178c54fda91a1bf5403eeb4c8593
Aitor <aitor.san.rodriguez@gmail.com>
2026-06-19 19:14:18 +0200

    Versionado

--- docs/notes.js
+++ docs/notes.js
@@ -1,5 +1,5 @@
 const NOTES_DATA = {
-  "title": "Actualización 0.1.0 - (Bienvenido Moraleda Variocolor)",
+  "title": "Actualización 0.1.2 - (Bienvenido Moraleda Variocolor)",
   "lines": [
     "Por razones operativas y que no haga falta avisar en cada arreglo menor, subo a la 0.1.0",
     "Guía incluida en el compendio.",
--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-var GAME_VERSION = '0.1.1';
+var GAME_VERSION = '0.1.2';
